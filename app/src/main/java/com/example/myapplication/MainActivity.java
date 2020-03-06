31789de02ad4e3997baec9eacd9ab17267bc637a
JhMateo <jhonderm.godoym@utadeo.edu.co>
2020-03-06 14:03:40 -0500

    Comentario en main prueba

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     //creamos las variables de tipo boton
     Button btnring ,btnreg;
-
+    //Cambios de pruebas por fin sirven o que
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
